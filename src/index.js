7aa701dda6e7e90fed3139418c47dba632f5b47f
Kelin Christi <christi.kelin20@gmail.com>
2016-06-08 16:14:46 -0700

    Issue with previous commit's account linking ~ fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var http = require('http'),
     AlexaSkill = require('./AlexaSkill'),
     APP_ID = 'YOUR APP ID FROM AMAZON',
-    OBA_KEY = 'YOUR API KEY FROM ONE BUS AWAY GOES HERE';
+    OBA_KEY = 'ONEBUSAWAY API';
 
 // Defining the URL that makes the requests
 var url = function(stopId) {
